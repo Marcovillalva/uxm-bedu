ffe878daa7a3979399ca94f1d813b9aaee3493ed
mako <spiller31@hotmail.com>
2019-10-30 16:05:24 -0600

    agrego componente contacto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Layout from './Components/Layout/Home';
 import Services from './Components/Layout/Services';
+import Contacto from './Components/Layout/Contacto';
 
 function App() {
   return (
     <div>
-      <Services/>
+      <Contacto/>
     </div>
   );
 }
---
+++ src/Components/Layout/Contacto.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Contacto extends Component {
+    render() {
+        return (
+            <div>
+                hey
+            </div>
+        );
+    }
+}
+
+export default Contacto;
