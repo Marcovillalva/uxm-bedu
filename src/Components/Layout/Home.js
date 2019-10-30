6cb72682349decf52441782e89ab3a5a3e2bf624
mako <spiller31@hotmail.com>
2019-10-30 12:26:11 -0600

    creo base paginas

--- src/Components/Layout/Home.js
+++ src/Components/Layout/Home.js
@@ -4,8 +4,39 @@ class Layout extends Component {
     render() {
         return (
             <div className="main-wraper">
-                <div>
+               
+                <div className="mainCont-header">
 
+                    <div className="logo">
+                    </div>
+
+                    <div className="navbar">
+                        <ul>
+                            <li>
+                                <a>Home</a>
+                            </li>
+                            <li>
+                                <a>Servicios</a>
+                            </li>
+                            <li>
+                                <a>Contacto</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+
+                <div className="mainCont-section">
+                    <div className="section-one">
+                        <h1>Transformamos espacios y creamos atmosferas</h1>
+                        <p>Transformar un espacio es determinante para 
+                            generar una conexión especial con las personas 
+                            y su entorno.</p>
+                    </div>
+                </div>
+
+                <div className="mainCont-footer">
+                        © 2019 Uxmal Stream
                 </div>
             </div>
         );
